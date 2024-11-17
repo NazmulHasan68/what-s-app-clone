2254abb37842b2d7f76eb4ff417314ce7d38480e
NazmulHasan68 <aishadowzone@gmail.com>
2024-11-18 03:03:16 +0600

    empty messaging interface

--- client/src/comopents/chat/chat/EmptyChat.jsx
+++ client/src/comopents/chat/chat/EmptyChat.jsx
@@ -1,8 +1,14 @@
+import empty from '../../../assets/em.png'
 
 export default function EmptyChat() {
   return (
-    <div>
-      emptyCHat
+    <div className='bg-[#f8f9f8] w-full h-screen flex justify-center items-center flex-col'>
+      <img src={empty} className=' mix-blend-multiply w-96 '/>
+      <p className='text-3xl font-normal text-[#415258] mt-5 py-2'>WhatsApp Web <span className=' bg-slate-300 text-sm p-1'>NEW</span></p>
+      <p className='text-sm text-gray-500 mt-4'>Now send and receive messages without keeping your phone online.</p>
+      <p className='text-sm text-gray-500'>Use WhatsAPp on up to 4 linked devices and 1 phone at the same time</p>
+      <div className='border-b-2 border-gray-300 w-[50%] py-5'></div>
+      <p className='text-sm text-gray-500 mt-2'>Make Calls from desktop with WhatsApp for Windows. Get ithere</p>
     </div>
   )
 }
